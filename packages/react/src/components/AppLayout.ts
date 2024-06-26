94b9651404182aa99bc5eedd957f6e21014ab859
Pavi Ganesh <pavithrahganesh@gmail.com>
2024-06-26 23:45:35 +0200

    Fix build

--- packages/react/src/components/AppLayout.ts
+++ packages/react/src/components/AppLayout.ts
@@ -1,16 +1,11 @@
 import { element } from '@lumus/engine';
 
-import Header from './Header/Header';
-
 const AppLayout = element('div', {
     name: 'LumusAppLayout',
     classNames: {
         variants: {
             inline: 'inline',
         },
-    },
-    slots: {
-        header: Header,
     },
 });
 
